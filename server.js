5670d50730d75ae0c24a165a8db824a597a1b272
faisal-dev786 <fasinadeem108@gmail.com>
2026-09-22 00:57:34 +0500

    api health added

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://127.0.0.1:5501'
 ];
+app.get("/api/health", publicRateLimiter, (req, res) => {
+  res.status(200).json({ message: "API is running" });
+});
 
 app.use(
     cors({
